cf878e2f7f4cd4f02a3e0cd5a72f4cf82931c8bd
Sarah Creager <sarah.f.jamieson@gmail.com>
2021-08-23 22:31:25 -0700

    updated README, finished adding features for lab06

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Component } from 'react';
 import axios from 'axios';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Image from 'react-bootstrap/Image';
+import Navbar from 'react-bootstrap/Navbar';
+
 
 
 export default class App extends Component {
@@ -31,14 +37,31 @@ export default class App extends Component {
   render() {
     return (
       <>
-        <input onChange={(e) => this.setState({ exploreQuery: e.target.value })} value={this.state.exploreQuery} placeholder="search for a city"></input>
-        <Button onClick={this.handleButtonClick} variant="primary" size="lg">Explore!</Button>{' '}
-        {this.state.error ? <h2>not a valid location</h2>
-          : this.state.location.place_id &&
+        <Container id="navBar">
+          <Navbar>
+            <Navbar.Brand href="#home" id="title">City Explorer</Navbar.Brand>
+          </Navbar>
+        </Container>
+        
+        <Container id="form">
+          <FloatingLabel controlId="floatingInputGrid" label="search for a city">
+            <Form.Control id="formInput" onChange={(e) => this.setState({ exploreQuery: e.target.value })} value={this.state.exploreQuery} type="city" placeholder="search for a city" />
+          </FloatingLabel>
+          <Button onClick={this.handleButtonClick} id="button" size="lg">Explore!</Button>
+        </Container>
+      
+        {' '}
+        <Container id="map">
+          {this.state.error ? <h2>not a valid location</h2>
+            : this.state.location.place_id &&
           <>
-            <h2>The city is: {this.state.location.display_name} The latitude is: {this.state.location.lat} The latitude is: {this.state.location.lon} </h2>
-            <img src={this.state.image} alt="map"></img>
+            <h2>City Name: {this.state.location.display_name}</h2> 
+            <h3>Latitude: {this.state.location.lat}.</h3> 
+            <h3>Longitude: {this.state.location.lon}. </h3>
+            <Image src={this.state.image} alt="map" thumbnail></Image>
           </>}
+        </Container>
+        <footer>By Sarah Creager</footer>
       </>
     );
   }
@@ -46,3 +69,6 @@ export default class App extends Component {
 
 
 
+
+
+{/* <input onChange={(e) => this.setState({ exploreQuery: e.target.value })} value={this.state.exploreQuery} placeholder="search for a city"></input> */}
