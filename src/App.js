df92db740b43bd6a277b00897a17cc5bb80f74ed
Sarah Creager <sarah.f.jamieson@gmail.com>
2021-08-23 22:33:38 -0700

    fixed netlify spacing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Component } from 'react';
 import axios from 'axios';
 import Button from 'react-bootstrap/Button';
@@ -21,7 +21,7 @@ export default class App extends Component {
       error:'',
     };
   }
- 
+
   handleButtonClick = async () => {
     try {
       const API_URL = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${this.state.exploreQuery}&format=json`;
@@ -33,7 +33,7 @@ export default class App extends Component {
       this.setState({error: true});
     }
   }
-  
+
   render() {
     return (
       <>
@@ -42,21 +42,21 @@ export default class App extends Component {
             <Navbar.Brand href="#home" id="title">City Explorer</Navbar.Brand>
           </Navbar>
         </Container>
-        
+
         <Container id="form">
           <FloatingLabel controlId="floatingInputGrid" label="search for a city">
             <Form.Control id="formInput" onChange={(e) => this.setState({ exploreQuery: e.target.value })} value={this.state.exploreQuery} type="city" placeholder="search for a city" />
           </FloatingLabel>
           <Button onClick={this.handleButtonClick} id="button" size="lg">Explore!</Button>
         </Container>
-      
+
         {' '}
         <Container id="map">
           {this.state.error ? <h2>not a valid location</h2>
             : this.state.location.place_id &&
           <>
-            <h2>City Name: {this.state.location.display_name}</h2> 
-            <h3>Latitude: {this.state.location.lat}.</h3> 
+            <h2>City Name: {this.state.location.display_name}</h2>
+            <h3>Latitude: {this.state.location.lat}.</h3>
             <h3>Longitude: {this.state.location.lon}. </h3>
             <Image src={this.state.image} alt="map" thumbnail></Image>
           </>}
